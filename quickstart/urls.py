0bf244dc89e01b157c1639543b8feb54c10e74fc
TomekM <tomasz_matuszewski@o2.pl>
2020-10-04 21:05:58 +0200

    viewests and routers finished

--- quickstart/urls.py
+++ quickstart/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
-from .views import ArticleGenericAPIView
+from .views import ArticleGenericAPIView, ArticleViewSet
+
+router = DefaultRouter()
+router.register("articles", ArticleViewSet, basename="articles")
 
 urlpatterns = [
-    path("article/<int:id>", ArticleGenericAPIView.as_view()),
-    path("articles/", ArticleGenericAPIView.as_view()),
+    # path("article/<int:id>", ArticleGenericAPIView.as_view()),
+    # path("articles/", ArticleGenericAPIView.as_view()),
+    path('viewset/', include(router.urls)),
+    path('viewset/<int:pk>/', include(router.urls)),
 ]
--- quickstart/views.py
+++ quickstart/views.py
@@ -2,7 +2,10 @@ from django.contrib.auth.models import Group, User
 from rest_framework import generics
 from rest_framework import mixins
 from rest_framework import permissions, viewsets
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 from quickstart.serializers import (ArticleSerializer, GroupSerializer,
                                     UserSerializer)
@@ -29,6 +32,36 @@ class GroupViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
 
+class ArticleViewSet(viewsets.ViewSet):
+
+    def list(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = ArticleSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        queryset = Article.objects.all()
+        article = get_object_or_404(queryset, pk=pk)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        article = Article.objects.get(pk=pk)
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+
 class ArticleGenericAPIView(generics.GenericAPIView, mixins.ListModelMixin,
                             mixins.CreateModelMixin, mixins.UpdateModelMixin,
                             mixins.RetrieveModelMixin, mixins.DestroyModelMixin):
@@ -36,7 +69,8 @@ class ArticleGenericAPIView(generics.GenericAPIView, mixins.ListModelMixin,
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
     lookup_field = 'id'
-    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    # authentication_classes = [SessionAuthentication, BasicAuthentication]
+    authentication_classes = [TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request, id=None):
